ec92f7e5e91222006caed16f7cd47da29f417625
Falmeea <connordhanson@gmail.com>
2024-04-19 20:07:24 +0100

    Add static files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from views import landing
+from flask_frozen import Freezer
 
 app = Flask(__name__)
 app.register_blueprint(landing, url_prefix="/")
@@ -7,3 +8,12 @@ app.register_blueprint(landing, url_prefix="/")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/')
+def index():
+    return 'Hello, this is my Flask app converted to a static site!'
+
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
