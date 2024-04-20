d0258030de2168cbb306fe4213b598bad8a63970
Falmeea <connordhanson@gmail.com>
2024-04-20 13:57:11 +0100

    removed all changes to views, app

--- views.py
+++ views.py
@@ -5,8 +5,3 @@ landing = Blueprint("landing", __name__)
 @landing.route("/")
 def home():
     return render_template("landing.html")
-
-# Route for the second page (Homepage)
-@landing.route("/home")
-def homepage():
-    return render_template("homepage.html")
